9b3f63a2bbdd9ef7df7ac7d769189350ff74660a
phinihas30 <phinnu@gmail.com>
2025-07-15 19:15:37 +0530

    Remove Click import and registration from admin.py (model no longer exists)

--- amazon/api/admin.py
+++ amazon/api/admin.py
@@ -1,5 +1,4 @@
 from django.contrib import admin
-from .models import AffiliateProduct, Click
+from .models import AffiliateProduct
 
 admin.site.register(AffiliateProduct)
-admin.site.register(Click)
