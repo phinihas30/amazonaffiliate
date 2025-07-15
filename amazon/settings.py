9fe5d77981c0646afcc6d447e78246aacf466ff5
phinihas30 <phinnu@gmail.com>
2025-07-15 19:49:13 +0530

    Fix import error: use 'amazon.api' in INSTALLED_APPS

--- amazon/settings.py
+++ amazon/settings.py
@@ -44,7 +44,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'api',
+    'amazon.api',
     'rest_framework',
     'corsheaders',
 ]
